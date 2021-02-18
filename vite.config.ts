004be449c99a4469d729fb5e865f7697100b7414
blessyou <370732889@qq.com>
2021-02-18 15:42:15 +0800

    feat: update vue

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx({})],
+  server: {
+    proxy: {
+      "/api": "https://happysooner.com",
+    },
+  },
 });
