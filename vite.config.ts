7ef4227a3b7cd5843164b72e33695fa0a8368600
blessyou <370732889@qq.com>
2021-02-25 13:51:06 +0800

    build: update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: process.env.BASE_URL || "/",
     plugins: [vue(), vueJsx({})],
     server: {
         proxy: {
