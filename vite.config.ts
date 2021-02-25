63a50433b32945808f4b26637eaefac42bf04d7a
unliar <i@happysooner.com>
2021-02-25 22:58:41 +0800

    build: add legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import legacy from "@vitejs/plugin-legacy";
 import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     base: process.env.BASE_URL || "/", // 静态资源地址 一般情况下是 cdn 地址
-    plugins: [vue(), vueJsx({})],
+    plugins: [legacy(), vue(), vueJsx({})],
     server: {
         proxy: {
             "/api": "https://happysooner.com",
