6e975d8e5acc87d2ec38d4cfe704d6d547e373dd
blessyou <370732889@qq.com>
2021-02-26 14:46:37 +0800

    refactor: remove store log

--- src/store/index.ts
+++ src/store/index.ts
@@ -47,20 +47,14 @@ const Store = createStore<StoreType>({
         // 获取认证用户信息
         async [ACTIONS.GET_AUTHED_USER_INFO](ctx) {
             const r = await GetUserInfo();
-            const data = r?.Result;
-            if (data) {
-                console.info("用户信息获取成功", data);
-                ctx.commit(MUTATIONS.UPDATE_USER_STORE, data);
-            }
+            const data = r?.Result ?? {};
+            ctx.commit(MUTATIONS.UPDATE_USER_STORE, data);
         },
         // 获取主题列表
         async [ACTIONS.GET_CATEGORY_LIST](ctx) {
             const r = await GetCategories();
-            const data = r?.Result;
-            if (data) {
-                console.log("获取主题信息");
-                ctx.commit(MUTATIONS.UPDATE_CATEGORY_LIST, data?.List ?? []);
-            }
+            const List = r?.Result?.List ?? [];
+            ctx.commit(MUTATIONS.UPDATE_CATEGORY_LIST, List);
         },
     },
 });
