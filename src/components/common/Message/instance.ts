f4d40cea973b004bf11e71140a0c200a38dd79ab
blessyou <370732889@qq.com>
2021-07-08 19:59:53 +0800

    feat: add close

--- src/components/common/Message/instance.ts
+++ src/components/common/Message/instance.ts
@@ -1,12 +1,13 @@
-import { createApp, getCurrentInstance } from "vue";
+import { createVNode, render } from "vue";
 import Ins from "./index.vue";
 import type { MessageInstance, Options } from "./type";
 const Message: MessageInstance = (opt?: Options) => {
     const cfg = opt || {};
-    const app = createApp(Ins, { ...cfg });
-    const dom = document.createElement("div");
-    dom.className = "happy__message";
-    const t = app.mount(dom);
+    const vm = createVNode(Ins, { ...cfg });
+    const container = document.createElement("div");
+    container.className = "happy__message";
+    render(vm, container);
+    document.body.appendChild(container);
 };
 
 Message.closeAll = () => {};
